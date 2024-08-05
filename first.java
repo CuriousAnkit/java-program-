d2e7dcf48338d0d987b4b9181aff335fd89ce759
Ankit Kumar <ankit8809@outlook.com>
2024-08-05 13:14:36 +0530

    Version 1

---
+++ first.java
@@ -0,0 +1,10 @@
+public class first {
+  public static void main(String args[]){
+    int num = 10;
+    int sum = 10;
+    System.out.println("Hello World!");
+    System.out.println("Ankit Kumar");
+    System.out.println( "numbe is " +(num + sum));
+    System.out.println("Today is monday");
+  }
+}
